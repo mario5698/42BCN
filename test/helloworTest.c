18cd9035ff4cdab5982d24c15ca4da2bea2c3e5e
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-03-04 23:09:47 +0100

    simple test

---
+++ test/helloworTest.c
@@ -0,0 +1,17 @@
+// include test definitions
+#include "simplectest/tests.h"
+#include "helloworld.h"
+
+// Start the overall test suite
+START_TESTS()
+
+    START_TEST("Hello World Test")
+       ASSERT_EQUALS("Hello World!", HelloWorld())
+    END_TEST()
+
+    START_TEST("Count World")
+        ASSERT_EQUALS(12, strlen(HelloWorld()))
+    END_TEST()
+
+// End the overall test suite
+END_TESTS()
