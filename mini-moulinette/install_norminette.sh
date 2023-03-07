972153d3e99e689d324554ccca250b471175fc76
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-03-07 09:55:46 +0100

    mini moulinette

---
+++ mini-moulinette/install_norminette.sh
@@ -0,0 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install python@3.11 
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install norminette
---
+++ mini-moulinette/mini-moul.sh
@@ -0,0 +1,32 @@
+source ~/Desktop/github/mini-moulinette/mini-moul/config.sh
+
+function handle_sigint {
+  echo "${RED}Script aborted by user. Cleaning up..."
+  rm -R ../mini-moul
+  echo ""
+  echo "${GREEN}Cleaning process done.${DEFAULT}"
+  exit 1
+}
+
+if [[ "$#" -eq 1 ]]; then
+	cp -R ~/Desktop/github/mini-moulinette/mini-moul mini-moul
+  run_norminette
+  trap handle_sigint SIGINT
+	cd mini-moul
+  ./test.sh "$1"
+  rm -R ../mini-moul
+else
+  printf "${RED}You need to choose an assignment. e.g: mini C02\n${DEFAULT}"
+
+fi
+
+exit 1
+
+run_norminette()
+{
+    if command -v norminette &> /dev/null; then
+        norminette
+    else
+        echo "norminette not found, skipping norminette checks"
+    fi
+}
