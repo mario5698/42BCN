6f90511baaf239f7539cf898a9bc1f686c80243f
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-03-08 13:59:36 +0100

    done

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marancib <marancib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 17:02:09 by marancib          #+#    #+#             */
+/*   Updated: 2023/02/14 15:32:52 by marancib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+
+/*int main(){
+	ft_print_alphabet();
+	return(0);
+}*/
